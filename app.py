173a22f74ce25a51666efad60de60b1da06a7931
Gokul <117812787+Gokulchandr@users.noreply.github.com>
2026-03-20 08:25:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from langchain_huggingface import HuggingFaceEndpoint
-
+from langchain_huggingface import ChatHuggingFace
 # -----------------------------
 # Configuration
 # -----------------------------
